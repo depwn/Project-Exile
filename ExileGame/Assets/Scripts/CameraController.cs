a22b174b184c377878a1ff2b081db6c404977e39
AggelosMpr <13-12795@saeinstitute.edu>
2020-11-25 20:30:48 +0200

    BasicTestCamera

---
+++ ExileGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Vector3 cameraOffset;
+    [SerializeField]
+    Vector3 cameraAngle;
+    [SerializeField]
+    float zoomSpeed;
+    [SerializeField]
+    float zoomFactor = 5f;
+    [SerializeField]
+    float minZoomFactor=2f;
+    [SerializeField]
+    float maxZoomFactor=10f;
+  
+    void Update()
+    {
+        zoomFactor -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoomFactor = Mathf.Clamp(zoomFactor, minZoomFactor, maxZoomFactor);
+       
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position + cameraOffset * zoomFactor;
+        transform.LookAt(target.position + cameraAngle);
+    }
+}
