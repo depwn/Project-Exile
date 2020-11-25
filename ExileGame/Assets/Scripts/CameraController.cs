d184db5967dea604a30e57478a464e8ab3c5b996
AggelosMpr <13-12795@saeinstitute.edu>
2020-11-25 21:19:18 +0200

    DMtriggerFloor

--- ExileGame/Assets/Scripts/CameraController.cs
+++ ExileGame/Assets/Scripts/CameraController.cs
@@ -18,16 +18,35 @@ public class CameraController : MonoBehaviour
     float minZoomFactor=2f;
     [SerializeField]
     float maxZoomFactor=10f;
+    [SerializeField]
+    private float rotationSpeed=100f;
+
+    private float rotationAng;
   
     void Update()
     {
+
         zoomFactor -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         zoomFactor = Mathf.Clamp(zoomFactor, minZoomFactor, maxZoomFactor);
-       
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            rotationAng += rotationSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+
+            rotationAng -= rotationSpeed * Time.deltaTime;
+        }
     }
     void LateUpdate()
     {
+
         transform.position = target.position + cameraOffset * zoomFactor;
         transform.LookAt(target.position + cameraAngle);
-    }
+
+        transform.RotateAround(target.position, Vector3.up, rotationAng);
+    }   
 }
