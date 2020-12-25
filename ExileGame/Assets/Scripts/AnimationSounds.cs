5111e24c1aff54b8610639311b3ea21d83fe213d
George.P <13-12738@saeinstitute.edu>
2020-12-25 16:43:05 +0200

    test push only animation sound script added no sound files

---
+++ ExileGame/Assets/Scripts/AnimationSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSounds : MonoBehaviour
+{
+    AudioSource AnimSoundSrc;
+    [SerializeField]
+    private AudioClip walkClip, hitClip;
+
+    private void Start()
+    {
+        AnimSoundSrc = GetComponent<AudioSource>();
+    }
+
+    private void WalkSound()
+    {
+        //AnimSoundSrc.Play();
+        AudioSource.PlayClipAtPoint(walkClip, transform.position);
+    }
+
+    private void HitSound()
+    {
+        AudioSource.PlayClipAtPoint(hitClip, transform.position);
+    }
+
+    private void ChopSound()
+    {
+
+    }
+
+    private void MineSound()
+    {
+
+    }
+}
