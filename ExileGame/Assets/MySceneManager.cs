2d792de115a1ee6d1be42194d458157b66070b04
Aggelos <13-12795@saeinstitute.edu>
2020-12-27 20:36:49 +0200

    ReloadSceneButton/Item size in inventory fixed

---
+++ ExileGame/Assets/MySceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{
+   
+    public void OnReloadSceneButton()
+    {
+        SceneManager.LoadScene("Main Game Scene");
+    }
+}
