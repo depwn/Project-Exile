d84c61aa80c928b0b2d39d0831109db27049aba8
Aggelos <13-12795@saeinstitute.edu>
2020-12-29 11:35:56 +0200

    Build_Added

---
+++ ExileGame/Assets/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public InventorySO playerInventory;
+    public void OnReloadSceneButton()
+    {
+        SceneManager.LoadScene("Main Game Scene");
+        playerInventory.InventoryContainer.Clear();
+    }
+}
