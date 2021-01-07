402e020264d82cd2978114f5f0f21605ec3ba571
Aggelos <13-12795@saeinstitute.edu>
2021-01-07 13:38:50 +0200

    Toggle inventory with B/Simple Menu

--- ExileGame/Assets/UIManager.cs
+++ ExileGame/Assets/UIManager.cs
@@ -6,9 +6,42 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public InventorySO playerInventory;
+    public GameObject inventoryUI;
+    public GameObject menuUI;
+    bool inventoryUIEnabled = false;
+
+    public void Update()
+    {
+        if (inventoryUIEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                inventoryUI.SetActive(false);
+                inventoryUIEnabled = false;
+            }
+        }
+        else if (!inventoryUIEnabled)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                inventoryUI.SetActive(true);
+                inventoryUIEnabled = true;
+            }
+        }
+    }
     public void OnReloadSceneButton()
     {
         SceneManager.LoadScene("Main Game Scene");
         playerInventory.InventoryContainer.Clear();
     }
+    public void LoadMenu()
+    {
+        menuUI.SetActive(true);
+    }
+    public void QuitMenu()
+    {
+        menuUI.SetActive(false);
+    }
 }
+
+
